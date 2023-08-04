598e95d8f805bc9547c9c43b79f69f43abd10090
Phoenix-Foxwell <analogsombra@gmail.com>
2023-08-04 08:01:14 +0530

    first commit

---
+++ settings/options.lua
@@ -0,0 +1,59 @@
+local options = {
+
+	-- number settings
+	number = true,
+	relativenumber = true,
+	-- numberwidth = 1,
+
+	-- tab settings
+	expandtab = true,
+	showtabline = 4,
+	tabstop = 4,
+	softtabstop = 4,
+	shiftwidth = 4,
+
+	-- indent settings
+	smartindent = true,
+    autoindent = true,
+
+
+    -- search settings
+    hlsearch = true,
+    incsearch = true,
+    
+
+
+	showmode = true,
+
+
+	
+
+	cmdheight = 1,
+    signcolumn = "yes",
+    wrap = true,
+    linebreak = true,
+    scrolloff = 8,
+    undofile = true,
+    smartcase = true,
+    
+    swapfile = false,
+    backup = false,
+    writebackup = false,
+
+
+
+    -- split window
+    splitbelow = true,
+    splitright = true,
+
+
+    -- highlights
+    cursorline = true,
+    cursorcolumn = true,
+
+}
+
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
