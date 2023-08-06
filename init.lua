b5b87e796635020b15dedcfebe0c3220e5e72d75
Phoenix-Foxwell <analogsombra@gmail.com>
2023-08-06 18:17:27 +0530

    packer to lazy.nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-require "settings.options"
-require "settings.keymap"
-require "settings.plugins"
-require "settings.colors"
+require "options"
+require "keymap"
+require "lazyplugins"
---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,28 @@
+return {
+    'catppuccin/nvim',
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            background = {
+                light = "latte",
+                dark = "mocha"
+            },
+            transparent_bakground = true,
+            show_end_of_buffer = false,
+            term_colors = true,
+            dim_inactive = {
+                enabled = false,
+                shade = "dark",
+                percentage = 0.15,
+            },
+            no_italic = false,
+            no_bold = false,
+            no_underline = false,
+        })
+        local colorscheme = "catppuccin"
+        local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+        if not status_ok then
+            return
+        end
+    end
+}
