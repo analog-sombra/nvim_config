410e164877a694c0ae33fe5829dfdb7659814c89
Phoenix-Foxwell <analogsombra@gmail.com>
2023-08-11 09:44:22 +0530

    removed wilder plugin

--- lua/plugins/uiutils.lua
+++ lua/plugins/uiutils.lua
@@ -141,6 +141,10 @@ return {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+            'rcarriga/nvim-notify'
+        },
         opts = {
             lsp = {
                 override = {
@@ -167,6 +171,7 @@ return {
                 command_palette = true,
                 long_message_to_split = true,
                 inc_rename = true,
+                lsp_doc_border = true,
             },
         },
         -- stylua: ignore
